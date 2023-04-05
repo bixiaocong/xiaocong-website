dd13bbd989cb09594ef18d75fe28aecbe144ccab
bixiaocong <bicongcong@xiaomi.com>
2023-04-05 23:33:10 +0800

    提交最新

--- sidebars.js
+++ sidebars.js
@@ -26,12 +26,12 @@ const sidebars = {
     },
   ],
    */
-  opinionSidebar: ['my-opinion/to-my-34', 'my-opinion/move', 'my-opinion/2022Review', 'my-opinion/2023Flag'],
+  opinionSidebar: ['my-opinion/to-my-34', 'my-opinion/move', 'my-opinion/2022Review', 'my-opinion/2023Flag', 'my-opinion/dairy'],
   boardGameBar: ['board-game/recommendation1', 'board-game/recommendation2'],
   readSidebar: ['reading/reading', 'reading/TheMoonandSixpence', 'reading/Frog', 'reading/2022Booklist'],
   podcastSidebar: ['podcast/recommend', 'podcast/culture', 'podcast/comedy', 'podcast/invest'],
   movieSidebar: ['movie/index', 'movie/Parasite'],
-  investSidebar: ['invest/card']
+  investSidebar: ['invest/card', 'invest/knowledge']
 };
 
 module.exports = sidebars;
